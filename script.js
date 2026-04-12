4893abccc1cf88fdf459554e40c73469a6a3f1cd
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-04-12 14:22:12 +0530

    React Components

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 import React from "react";
+import ReactDOM from "react-dom/client";   
 
-import ReactDOM from "react-dom/content";
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading", xyz: "abc" },
+//     "Hello World from React!"
+// );
 
-const heading=React.createElement(
-    "h1",
-    
-        {id:"heading" ,xyz:"abc"},
-        "Hello World from React!"
-    
-);
+// console.log(heading);
 
-console.log(heading);
-const root=ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+
+
+
+// react  component
+const Title = () =>(
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+    );
+
+    const HeadingComponent =()=>(
+        <div id="container">
+            <Title />
+            <h1 className="heading">Namaste React Functional Component</h1>
+        </div>
+    );
+
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    root.render(<HeadingComponent/>);
