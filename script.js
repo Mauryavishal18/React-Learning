2fa15c799dbdb20a9d7d1ca2e12e943173e84ab0
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-04-13 22:58:33 +0530

    creating basic cards creation ,header and footer in pageLayout

--- script.js
+++ script.js
@@ -1,33 +1,91 @@
 import React from "react";
-import ReactDOM from "react-dom/client";   
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading", xyz: "abc" },
-//     "Hello World from React!"
-// );
+// Header Component
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn-icons-png.flaticon.com/512/3075/3075977.png"
+        />
+      </div>
 
-// console.log(heading);
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// Restaurant Card
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/PC_Mweb/Salad.png"
+      />
+      <h3>Meghana Foods</h3>
+      <p>Biryani, North Indian, Asian</p>
+      <p>4.4 ⭐</p>
+      <p>38 minutes</p>
+    </div>
+  );
+};
 
-// root.render(heading);
+// Body
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+
+      <div className="res-container">
+        <RestaurantCard />
+         <RestaurantCard />
+          <RestaurantCard />
+           <RestaurantCard />
+            <RestaurantCard />
+             <RestaurantCard />
+              <RestaurantCard />
+               <RestaurantCard />
+                <RestaurantCard />
+                 <RestaurantCard />
+                  <RestaurantCard />
+                   <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+// App Layout
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer/>
+    </div>
+  );
+};
 
 
 
-// react  component
-const Title = () =>(
-    <h1 className="head" tabIndex="5">
-        Namaste React using JSX
-    </h1>
-    );
 
-    const HeadingComponent =()=>(
-        <div id="container">
-            <Title />
-            <h1 className="heading">Namaste React Functional Component</h1>
-        </div>
-    );
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4>© 2026 Food App</h4>
+      <p>Made with ❤️ by Vishal Maurya</p>
+    </div>
+  );
+};
 
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(<HeadingComponent/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
